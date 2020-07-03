ae6c16305aa079b5009f7bdf61c90638b98397f8
Greg Scott <gw.scott@virgin.net>
2020-07-03 11:21:20 +0100

    Adding files for multi-vm setup

--- scripts/centos-web.sh
+++ scripts/centos-web.sh
@@ -14,14 +14,6 @@ service httpd start
 # PHP:
 yum install -y php php-cli php-common php-devel php-mysql
 
-# MySQL:
-
-yum install -y mysql mysql-server mysql-devel
-chkconfig --add mysqld
-chkconfig mysqld on
-service mysqld start
-mysql -u root -e "SHOW DATABASES";
-
 
 # Download starter content:
 cd /vagrant
